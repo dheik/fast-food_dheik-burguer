67dd0e456fd5997e521210d663faefdc125920d6
dheik <dheik387387@gmail.com>
2025-02-21 17:36:24 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products Page</h1>
+      <button>Click me!</button>
+      <input placeholder="Bora fechar esse projeto" />
+    </div>
+  );
+};
+
+export default ProductPage;
