3b981dee83853065b6846bcddd8a6b015da70a85
dheik <dheik387387@gmail.com>
2025-02-21 17:38:20 -0300

    feat: add shadcn initial setup and components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 const ProductPage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
       <h1 className="text-red-500">Products Page</h1>
-      <button>Click me!</button>
-      <input placeholder="Bora fechar esse projeto" />
+      <Button>Click me!</Button>
+      <Input placeholder="Bora fechar esse projeto" />
     </div>
   );
 };
