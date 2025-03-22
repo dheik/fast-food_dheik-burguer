afaf06a3c0ff0d1ee2c370f80ad092fb0cd18300
dheik <dheik387387@gmail.com>
2025-03-22 13:22:56 -0300

    chore: update accessible image for Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+    remotePatterns: [
+      { hostname: "u9a6wmr3as.ufs.sh" },
+      { hostname: "t4.ftcdn.net" }, // Added hostname
+    ],
   },
 };
 
